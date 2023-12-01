71a01ab1e932b4e5f5b15b01d450c75e675f6b5c
KiwoonHong <kiwoon2799@gmail.com>
2023-12-01 13:59:14 -0700

    load packages / data

--- script.R
+++ script.R
@@ -1,1 +1,8 @@
-Test
+if (!require("pacman"))
+  install.packages("pacman")
+pacman::p_load(tidyverse,here)
+pacman::p_load_gh()
+ggplot2::theme_set(ggplot2::theme_minimal
+                   (base_size = 14))
+
+top50 <- read.csv(here("data","top50.csv"))
